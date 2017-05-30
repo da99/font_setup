b82fc3192aed4a3fb04e23f37fcdfcc28d5e78f0
da99 <i-hate-spam-1234567@mailinator.com>
2017-05-30 11:56:33 -0500

    Added: list

--- bin/public/list/_.sh
+++ bin/public/list/_.sh
@@ -1,6 +1,8 @@
 
 # === {{CMD}}
 list () {
+  fc-list | cut -d':' -f2 | sort | uniq
+  exit 0
   local +x IFS=$'\n'
   for DIR in $(find "$THIS_DIR"/bin/public -maxdepth 1 -mindepth 1 -type d -name "install-font-*"); do
     local +x BASE="$(basename "$DIR")"
