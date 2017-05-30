13174a167b636fc3cc71548b673522217560d144
da99 <i-hate-spam-1234567@mailinator.com>
2017-05-30 18:44:16 -0500

    Added: install raleway

--- bin/public/install/_.sh
+++ bin/public/install/_.sh
@@ -5,12 +5,75 @@
 # === {{CMD}}  font.zip
 install () {
   local +x NAME="$1"; shift
+  local +x PATH="$PATH:$THIS_DIR/bin"
 
   case "$NAME" in
     *.zip|*.ZIP)
       local +x FILE="$(realpath "$NAME")"
       unzip "$FILE" -d "$HOME"/.fonts
       fc-cache -fv
+      ;;
+
+    raleway)
+      mkdir -p /tmp/google-fonts
+      cd       /tmp/google-fonts
+
+      for FILE in $(lynx --dump https://github.com/google/fonts/tree/master/ofl/raleway  | grep -P '\]Raleway.*.ttf' | cut -d']' -f2 | cut -d'[' -f1); do
+        wget -O "$FILE" "https://github.com/google/fonts/blob/master/ofl/raleway/${FILE}?raw=true"
+      done
+      cp -f *.ttf $HOME/.fonts
+      fc-cache -fv
+      ;;
+
+    SanFrancisco|"San Francisco"*)
+      cd /tmp
+      local +x SF_FONTS="https://github.com/AppleDesignResources/SanFranciscoFont"
+      if [[ -d "SanFranciscoFont" ]]; then
+        cd SanFranciscoFont
+        git pull
+      else
+        git clone "$SF_FONTS"
+        cd SanFranciscoFont
+      fi
+
+      mkdir -p "$HOME/.fonts"
+      cp -f *.otf "$HOME/.fonts"
+      fc-cache -fv
+      ;;
+
+    tecate-bitmap-font)
+      local +x NAME="$1"; shift
+      local +x EXT="$1"; shift
+      cd /tmp
+      if [[ -d bitmap-fonts ]]; then
+        cd bitmap-fonts
+        git pull
+      else
+        git clone https://github.com/Tecate/bitmap-fonts
+        cd bitmap-fonts
+      fi
+      cp bitmap/$NAME/**/*.$EXT "$HOME"/.fonts -f ||
+      cp bitmap/$NAME/*.$EXT "$HOME"/.fonts -f
+      fc-cache -fv
+      ;;
+
+    zevv-peep)
+      font_setup install tecate-bitmap-font $NAME bdf
+      ;;
+
+    knxt)
+      echo "NOTE: $NAME font best used at size 20 medium/normal" >&2
+      font_setup install tecate-bitmap-font $NAME {bdr,pcf}
+      ;;
+
+    ctrld-font)
+      echo "NOTE: $NAME font best used at size 13 medium/normal" >&2
+      font_setup install tecate-bitmap-font $NAME bdf
+      ;;
+
+    boxxy)
+      echo "NOTE: boxxy font best used at size 14 medium/normal" >&2
+      font_setup install tecate-bitmap-font $NAME bdf
       ;;
 
     --all)
