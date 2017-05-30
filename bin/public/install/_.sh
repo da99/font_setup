d89eda133ce13ff441f73aca11ac647410c853bd
da99 <i-hate-spam-1234567@mailinator.com>
2017-05-30 12:37:30 -0500

    Allow: install a zip file or a directory of zip files.

--- bin/public/install/_.sh
+++ bin/public/install/_.sh
@@ -2,9 +2,17 @@
 # === {{CMD}}  --all
 # === {{CMD}}  --list
 # === {{CMD}}  siji
+# === {{CMD}}  font.zip
 install () {
   local +x NAME="$1"; shift
+
   case "$NAME" in
+    *.zip|*.ZIP)
+      local +x FILE="$(realpath "$1")"; shift
+      unzip "$FILE" -d "$HOME"/.fonts
+      fc-cache -fv
+      ;;
+
     --all)
       source "$THIS_DIR"/bin/public/list/_.sh
       local +x IFS=$'\n'
@@ -14,6 +22,15 @@ install () {
       ;;
 
     *)
+      if [[ -d "$NAME" ]]; then
+        local +x DIR="$NAME"
+        local +x IFS=$'\n'
+        cd "$DIR"
+        unzip '*.zip' -d "$HOME"/.fonts
+        fc-cache -fv
+        exit 0
+      fi
+
       local +x DIR="$THIS_DIR"/bin/public/install-font-"$NAME"
       if [[ ! -d "$DIR" ]]; then
         echo "!!! Invalid name for font: $NAME" >&2
